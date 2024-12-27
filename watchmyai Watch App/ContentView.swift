860473edb2cba04d72b34b79c5b2b9052694b226
Sasan Rafat Nami <Nami.srn@gmail.com>
2024-12-27 18:34:54 +0100

    Initial Commit

---
+++ watchmyai Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchmyai Watch App
+//
+//  Created by Rafat Nami, Sasan on 27.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchmyai Watch App/watchmyaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchmyaiApp.swift
+//  watchmyai Watch App
+//
+//  Created by Rafat Nami, Sasan on 27.12.24.
+//
+
+import SwiftUI
+
+@main
+struct watchmyai_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
